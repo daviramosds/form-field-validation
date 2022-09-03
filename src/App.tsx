3ebc954d23b8110ca343146830cc60a973746937
daviramosds <davirds.dev@gmail.com>
2022-09-03 07:04:37 -0300

    input data validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import styled from "styled-components";
 import { Card } from "./components/Card";
 import { Input } from "./components/input";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+type FormData = {
+  email: String;
+  password: String;
+};
+
+const schema = yup
+  .object({
+    email: yup.string().email().required("email cannot be empty"),
+    password: yup.string().required("password cannot be empty"),
+  })
+  .required();
+
 function App() {
   const Container = styled.div`
     display: flex;
@@ -13,13 +29,36 @@ function App() {
     justify-content: center;
   `;
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
   return (
     <Container className="App">
-      <Card>
-        <Input type="email" placeholder="email" />
-        <Input type="password" placeholder="password" />
-        <Input type="submit" placeholder="submit" />
-      </Card>
+      <form onSubmit={onSubmit}>
+        <Card>
+          <Input
+            type="email"
+            placeholder="email"
+            register={register}
+            error={errors.email?.message}
+          />
+          <Input
+            type="password"
+            placeholder="password"
+            register={register}
+            // error={errors.password?.message}
+          />
+          <Input type="submit" placeholder="submit" />
+          <input type="submit" value="a" />
+        </Card>
+      </form>
     </Container>
   );
 }
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -3,12 +3,28 @@ import { DefaultInput, SubmitInput } from "./styles";
 interface InputPropsInterface {
   type: string;
   placeholder: string;
+  error?: string | undefined;
+  register?: any;
 }
 
-export function Input({ type, placeholder }: InputPropsInterface) {
+export function Input({
+  type,
+  placeholder,
+  register,
+  error = "",
+}: InputPropsInterface) {
   if (type === "submit") {
     return <SubmitInput value={placeholder} type="submit" />;
   }
 
-  return <DefaultInput placeholder={placeholder} type={type} />;
+  return (
+    <div>
+      <DefaultInput
+        {...register(placeholder)}
+        placeholder={placeholder}
+        type={type}
+      />
+      <label>{error}</label>
+    </div>
+  );
 }
